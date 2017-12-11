38891f6f1337af6b54918def09400177a5a22e9e
Janakikamal <janaki.510@gmail.com>
2017-12-11 23:08:52 +0530

    removed unwanted files

--- static/Q5.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/robots.txt/')
-def deny_request():
-    return 'You Should Not Be Here...Request Denied'
-if __name__ == "__main__":
-    app.run()
